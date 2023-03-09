d7990976b892a2e6b0e465e9292aec48f27ae53b
Ihor Kaniuk <ikanyuk@icloud.com>
2023-03-09 20:51:34 +0200

    update

--- js/trial.js
+++ js/trial.js
@@ -2,18 +2,26 @@
 // 1)Given a string of digits, you should replace any digit below 5 with '0' and
 // any digit 5 and above with '1'.Return the resulting string.
 
-function fakeBin(x) {
-  x = x.split('');
-  for (let i = 0; i < x.length; i += 1){
-    if (x[i] < 5) {
-      x[i] = 0;
-    } else {
-      x[i] = 1;
-    }
-  }
-  return x.join('');
+// function fakeBin(x) {
+//   x = x.split('');
+//   for (let i = 0; i < x.length; i += 1){
+//     if (x[i] < 5) {
+//       x[i] = 0;
+//     } else {
+//       x[i] = 1;
+//     }
+//   }
+//   return x.join('');
     
-}
+// }
 
-console.log(fakeBin('45385593107843568'));
-// 01011110001100111
+// 2)-------------------------------------------
+// поиск гласных букв
+//  const vowels = 'aeiou';
+//   let count = 0;
+//   for(let i = 0; i < str.length; i += 1){
+//     if(vowels.includes(str[i])){
+//       count += 1
+//     }
+//   }
+//   return count;
