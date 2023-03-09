cb5ed208effa361618e19802b1960485f084a0e6
Ihor Kaniuk <ikanyuk@icloud.com>
2023-03-09 17:13:24 +0200

    update

--- js/trial.js
+++ js/trial.js
@@ -1,8 +1,19 @@
-const target = 3;
-let sum = 0;
+// ---------------------------------CODEWARS---------------------------------
+// 1)Given a string of digits, you should replace any digit below 5 with '0' and
+// any digit 5 and above with '1'.Return the resulting string.
 
-for (let i = 0; i <= target; i += 1) {
-  sum += i;
+function fakeBin(x) {
+  x = x.split('');
+  for (let i = 0; i < x.length; i += 1){
+    if (x[i] < 5) {
+      x[i] = 0;
+    } else {
+      x[i] = 1;
+    }
+  }
+  return x.join('');
+    
 }
 
-console.log(sum);
+console.log(fakeBin('45385593107843568'));
+// 01011110001100111
