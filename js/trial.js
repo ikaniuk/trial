accabe54a33beb2c8db993798eff0ee9ad175666
Ihor Kaniuk <ikanyuk@icloud.com>
2023-03-11 17:04:40 +0200

    add-task

--- js/trial.js
+++ js/trial.js
@@ -124,3 +124,24 @@
 // }
 
 // console.log(filter_list([1, 2, 34, 'gh', '1']));
+
+
+// ----------------------------------------------------------------------
+// Trolls are attacking your comment section!
+
+// A common way to deal with this situation is to remove all of the
+// vowels from the trolls' comments, neutralizing the threat.
+
+// Your task is to write a function that takes a string and return
+// a new string with all vowels removed.
+
+// For example, the string "This website is for losers LOL!" would
+// become "Ths wbst s fr lsrs LL!".
+
+// function disemvowel(str) {
+//     for (let i = 0; i < str.length; i += 1){
+//         console.log(str[i]);
+//     }
+// }
+
+// disemvowel("This website is for losers LOL!")
