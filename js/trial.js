5ffe28bba7c6a2deed8073bfac61bcc0fcf29dab
Ihor Kaniuk <ikanyuk@icloud.com>
2023-03-14 21:22:29 +0200

    update

--- js/trial.js
+++ js/trial.js
@@ -151,3 +151,17 @@
 // }
 
 // console.log(disemvowel("This website is for losers LOL!"))
+
+// ----------------------------------------------------------------------
+// function getSum(a, b) {
+//     let min = Math.min(a, b);
+//     let max = Math.max(a, b);
+//     let sum = 0;
+//     for (let i = min; i <= max; i += 1){
+//         sum += i;
+//     }
+//     return sum
+// }
+
+// console.log(getSum(5, -1))
+// console.log(getSum(525, 197))
